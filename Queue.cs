dea2686f77aa3f6b2fbb24a6f3784259bed0ae16
Mark Dewey <mark.t.dewey@gmail.com>
2017-08-18 19:26:25 -0400

    reset

---
+++ Queue.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Generics
+{
+    // This is a FIFO structure, useful for notifications 
+    // and transactions and ordering tasks
+   
+}
